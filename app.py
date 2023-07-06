99538a56dd2ffaad4ed8d9e72f01abe8ad7385d6
bconti123 <bryantcconti@gmail.com>
2023-07-06 15:44:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, session
 from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, User, View
@@ -25,3 +25,36 @@ toolbar = DebugToolbarExtension(app)
 connect_db(app)
 
 db.create_all()
+
+@app.before_request
+def before_request_global():
+    """If visit any website routes, add guest user session to Flask global """
+    if not CURR_USER_KEY in session:
+        guest = User.guest_visit()
+        session[CURR_USER_KEY] = guest.id
+
+    if CURR_LOGIN_KEY in session:
+        del session[CURR_USER_KEY]
+    
+@app.before_request
+def add_user_to_g():
+    """ When we logged in, the website detect global login users """
+    if CURR_LOGIN_KEY in session:
+        g.user = User.session.get(session[CURR_LOGIN_KEY])
+    else:
+        g.user = None
+    
+    def do_login(user):
+        session[CURR_LOGIN_KEY] = user.id
+    
+    def do_logout(user):
+
+        if CURR_LOGIN_KEY in session:
+            del session[CURR_LOGIN_KEY]
+
+    
+
+
+@app.route('/')
+def homepage():
+    return render_template('/base.html')
