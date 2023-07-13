daeb48757868edaa776034ef33eb8b119b575864
bconti123 <bryantcconti@gmail.com>
2023-07-12 19:22:49 -0700

    Still fixing few issues

--- app.py
+++ app.py
@@ -143,27 +143,36 @@ def find_card_desc(card):
     cond_obj = {}
     cost_obj = {}
     act_obj = {}
+    # if ('toss a coin:' in desc_list):
+    #     desc_list = [' '.join(desc_list)]
     for desc in desc_list:
 
         if ":" in desc:
             cond = desc.index(":")
-
             substring = desc[0:cond+1]
-            
             cond_obj = {'condition' : substring}
             desc = desc.replace(substring, '')
+        else:
+            cond_obj = {}
+
         if ";" in desc:
             cost = desc.index(";")
             substring = desc[0:cost+1]
         
             cost_obj = {'cost' : substring}
             desc = desc.replace(substring, '')
+        else:
+            cost_obj = {}
+
+        
         
         act_obj = {'act': desc}
+
         # ISSUES: dot '●', Fix it later.
         result_list.append({**cond_obj, **cost_obj, **act_obj})
 
     return result_list
+
 
 # Card API Route
 @app.route('/cards')
