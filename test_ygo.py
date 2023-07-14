0b4bbfeeb9227ddcd46cd105b79ee8c1f4bcc3ea
bconti123 <bryantcconti@gmail.com>
2023-07-14 16:19:54 -0700

    Finished test_ygo.py

--- test_ygo.py
+++ test_ygo.py
@@ -22,13 +22,25 @@ class Ygo_Cards_TestCase(TestCase):
         self.assertIn(self.DMcard, name_list)
         self.assertIn("Dark Magician Girl", name_list)
 
-        
-
     def test_find_card_id(self):
+        id = 46986414
+        find = find_card_id(id)
+        card_id = find[0]['id']
 
-        id_list = [{card['id'] : card['name'}] for card in self.cards]
+        self.assertEqual(card_id, id)
+        
+    def test_find_card_desc(self):
+        card = find_card_id(40737112)
+        desc_list = find_card_desc(card)
+        found_cond = any('condition' in desc for desc in desc_list)
+        found_cost = any('cost' in desc for desc in desc_list)
+        found_act = any('act' in desc for desc in desc_list)
+        self.assertTrue(found_cond)
+        self.assertTrue(found_cost)
+        self.assertTrue(found_act)
 
-        find_card_id()
+
+
 
 
         
