ef096eda8aebe4333de532653567764d35801fe4
bconti123 <bryantcconti@gmail.com>
2023-07-19 18:17:11 -0700

    Updated app.py

--- app.py
+++ app.py
@@ -14,17 +14,6 @@ CURR_LOGIN_KEY = 'curr_login_user'
 app = Flask(__name__)
 app.app_context().push() # Flask latest version does need this.
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///yugioh_eff_checker'))
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "Its a secret :)")
-
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['FLASK_DEBUG'] = True
-toolbar = DebugToolbarExtension(app)
-
 connect_db(app)
 
 db.create_all()
